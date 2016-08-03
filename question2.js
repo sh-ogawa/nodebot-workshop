63a176cc9cbe047404baa58553df6baeb9e21311
sh-ogawa <ogawa.shogo@tis.co.jp>
2016-08-04 01:17:59 +0900

    Passing question 2.

---
+++ question2.js
@@ -0,0 +1,10 @@
+var five = require('johnny-five')
+var board = new five.Board()
+board.on('ready', function () {
+  var servo = new five.Servo(9);
+  servo.sweep( [0, 180] );
+  board.wait(3000, function(){
+    servo.stop();
+    servo.center();
+  });
+});
