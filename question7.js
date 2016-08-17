86a0030f7dd09a6cfc8dec85917ab844da803262
sh-ogawa <ogawa.shogo@tis.co.jp>
2016-08-18 04:54:56 +0900

    Remove unnecessary code.

--- question7.js
+++ question7.js
@@ -5,10 +5,7 @@ board.on('ready', function () {
   var potentiometer = new five.Sensor("A2");
   var servo = new five.Servo(9);
   servo.to(180);
-  board.repl.inject({
-    pot: potentiometer
-  });
-
+  
   potentiometer.on("change", function() {
     //potentiometerの現在の値をポテンションメータ(0-1023)とサーボモータ(0-179)の角度に変換する
     var position = five.Fn.map(this.value, 0, 1023, 0, 179);
