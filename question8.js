15ad9195682430bee0fc858d125b3f96fa528e6c
sh-ogawa <ogawa.shogo@tis.co.jp>
2016-08-18 04:57:26 +0900

    It was confirmed that the connect is succeed on my RCP client.

--- question8.js
+++ question8.js
@@ -1,16 +1,35 @@
 var five = require('johnny-five');
-var board = new five.Board();
 var dnode = require('dnode');
+var board = new five.Board();
 
 board.on('ready', function () {
-  var tempsensor = new five.Temperature("A0");
+  var tempsensor = new five.Thermometer({
+    controller: "TMP36",
+    pin: "A0"
+  });
+
+  var temp = null;
+  tempsensor.on('data', function(){
+    console.log(this.celsius)
+    temp = this.celsius;
+  });
 
   var server = dnode({
-    getTemperature : function(cb){
-        cb(tempsensor.celsius);
+    getTemperature : function (cb) {
+      console.log("request recieve");
+      cb(temp);
     }
   });
 
   server.listen(1337);
+
+  var d = dnode.connect(1337);
+  d.on('remote', function (remote) {
+    console.log("request send");
+    remote.getTemperature(function (s) {
+        console.log("response recieve :" + s);
+        d.end();
+    });
+});
 
 });
